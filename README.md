90444029e1754f177a44aeb1f8d0133e22b0d9ba
techstay <lovery521@gmail.com>
2020-02-11 00:29:13 +0800

    添加了wireguard配置脚本

--- README.md
+++ README.md
@@ -1,2 +1,51 @@
 # climbthewall
+
 你懂的
+
+## wireguard
+
+一个简易的wireguard配置脚本，使用前请确保系统已经安装好wireguard，并可以使用wg命令。如果已经存在`/etc/wireguard/wg0.conf`文件，或者已经使用`wg-quick up wg0`命令，请删除配置文件，并使用`wg-quick down wg0`关闭wg0，然后再运行本脚本。
+
+### 安装wireguard
+
+首先安装wireguard：
+
+```sh
+# Ubuntu 19.10及更新的系统
+$ sudo apt install wireguard
+
+# Ubuntu 19.04及更旧的系统
+$ sudo add-apt-repository ppa:wireguard/wireguard
+$ sudo apt-get update
+$ sudo apt-get install wireguard
+
+# 如果系统提示未找到add-apt-repository命令
+# 请先安装software-properties-common
+# 然后再运行add-apt-repository命令
+$ sudo apt-get install software-properties-common
+```
+
+其他系统安装请查看wireguard官方安装教程。
+
+### 运行脚本
+
+下载脚本，运行即可。密钥对及配置文件保存在`~/.wireguard/`下。
+
+```sh
+wget https://raw.githubusercontent.com/techstay/climbthewall/master/wg.sh
+bash wg.sh
+```
+
+### 使用客户端连接
+
+到[wireguard官网](https://www.wireguard.com/install/)下载客户端，将上面生成的客户端配置文件保存，导入客户端即可。
+
+手机客户端使用时，可以从`client.conf`生成二维码。
+
+```sh
+# 使用Python生成二维码，首先安装qrcode
+pip install qrcode[pil]
+
+# 生成二维码
+cat client.conf|qr wireguar-client.png
+```
---
+++ wg.sh
@@ -0,0 +1,59 @@
+#! /bin/bash
+
+config_dir="$HOME/.wireguard/"
+
+mkdir -p "$config_dir"
+cd "$config_dir" || {
+    echo 切换目录失败，程序退出
+    exit
+}
+# 生成两对密钥，分别用作服务器和客户端使用
+wg genkey | tee pri1 | wg pubkey >pub1
+wg genkey | tee pri2 | wg pubkey >pub2
+
+# 设置密钥访问权限
+chmod 600 pri1
+chmod 600 pri2
+
+interface=$(ip -o -4 route show to default | awk '{print $5}')
+ip=$(ip -4 addr show "$interface" | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+
+# 生成服务端配置文件
+cat >wg0.conf <<EOL
+[Interface] 
+PrivateKey = $(cat pri1)
+Address = 10.10.10.1
+ListenPort = 54321
+PostUp   = iptables -A FORWARD -i wg0 -j ACCEPT; iptables -A FORWARD -o wg0 -j ACCEPT; iptables -t nat -A POSTROUTING -o "$interface" -j MASQUERADE
+PostDown = iptables -D FORWARD -i wg0 -j ACCEPT; iptables -D FORWARD -o wg0 -j ACCEPT; iptables -t nat -D POSTROUTING -o "$interface" -j MASQUERADE
+[Peer]
+PublicKey = $(cat pub2)
+AllowedIPs = 10.10.10.2/32
+EOL
+
+# 生成客户端配置文件
+cat >client.conf <<EOL
+[Interface]
+PrivateKey = $(cat pri2)
+Address = 10.10.10.2
+DNS = 8.8.8.8
+
+[Peer]
+PublicKey = $(cat pub1)
+Endpoint = "$ip":54321
+AllowedIPs = 0.0.0.0/0
+EOL
+
+# 复制配置文件并启动
+sudo cp wg0.conf /etc/wireguard/ || {
+    echo 复制失败,请检查/etc/wireguard目录或wg0.conf是否已经存在
+    exit
+}
+sudo wg-quick up wg0 || {
+    echo 启动wireguard失败，请检查/etc/wireguard/wg0.conf是否存在错误
+    exit
+}
+
+# 显示客户端配置文件
+echo "----------以下是客户端配置文件，请保存并在客户端中使用----------"
+cat client.conf
